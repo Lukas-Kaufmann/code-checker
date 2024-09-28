2ec471d3354f5f24e433d5fe81bc5b78982a3718
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2024-09-28 22:22:41 +0200

    switch format, c

--- app/src/main/kotlin/at/fhv/lka2/checker/rules/SwitchFormatJavaRule.kt
+++ app/src/main/kotlin/at/fhv/lka2/checker/rules/SwitchFormatJavaRule.kt
@@ -2,17 +2,20 @@ package at.fhv.lka2.checker.rules
 
 import at.fhv.lka2.checker.config.RuleConfig
 import at.fhv.lka2.checker.getSourceText
+import at.fhv.lka2.checker.model.CRule
 import at.fhv.lka2.checker.model.JavaRule
 import at.fhv.lka2.checker.model.Violation
 import com.github.javaparser.ast.stmt.SwitchStmt
+import org.eclipse.cdt.core.dom.ast.IASTStatement
+import org.eclipse.cdt.core.dom.ast.IASTSwitchStatement
+import java.io.File
 
 data class SwitchFormatRuleConfig(override val enabled: Boolean = true) : RuleConfig
 
+private val switchPattern = "switch (\\S.*\\S) \\{(.|\\n)*\\}".toRegex()
 
 class SwitchFormatJavaRule(config: SwitchFormatRuleConfig = SwitchFormatRuleConfig()) :
     JavaRule<SwitchFormatRuleConfig>(config) {
-
-    private val singleIfPattern = "switch (\\S.*\\S) \\{(.|\\n)*\\}".toRegex()
 
     override fun visit(switchStmt: SwitchStmt, arg: MutableList<Violation>) {
         val file = switchStmt.findCompilationUnit().get().storage.get().path
@@ -21,9 +24,29 @@ class SwitchFormatJavaRule(config: SwitchFormatRuleConfig = SwitchFormatRuleConf
 
         val sourceText = file.getSourceText(start, end)
 
-        if (!sourceText.matches(singleIfPattern)) {
+        if (!sourceText.matches(switchPattern)) {
             arg.add(Violation(Violation.Location(file.toFile(), start.line), "Invalid format for switch statement"))
         }
         super.visit(switchStmt, arg)
+    }
+}
+
+class SwitchFormatCRule(config: SwitchFormatRuleConfig = SwitchFormatRuleConfig()): CRule<SwitchFormatRuleConfig>(config) {
+    override fun visit(statement: IASTStatement?): Int {
+        if (statement is IASTSwitchStatement) {
+            val sourceText = statement.getSourceText()
+            if(!sourceText.matches(switchPattern)) {
+                addViolation(
+                    Violation(
+                        Violation.Location(
+                            File(statement.fileLocation.fileName),
+                            statement.fileLocation.startingLineNumber
+                        ),
+                        "Invalid format for switch statement"
+                    )
+                )
+            }
+        }
+        return super.visit(statement)
     }
 }
---
+++ violations/src/main/c/SwitchStatementFormatViolations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    switch   (   i  )  {
+        case 3:
+            break;
+    }
+
+    switch (i) {
+        case 3:
+            break;
+    }
+}
