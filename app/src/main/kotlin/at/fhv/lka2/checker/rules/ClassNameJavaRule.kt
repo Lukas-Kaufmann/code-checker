947355df42327cfef050eb1a0e0f8c629af61421
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2024-09-29 10:19:22 +0200

    class name rule

---
+++ app/src/main/kotlin/at/fhv/lka2/checker/rules/ClassNameJavaRule.kt
@@ -0,0 +1,31 @@
+package at.fhv.lka2.checker.rules
+
+import at.fhv.lka2.checker.config.RuleConfig
+import at.fhv.lka2.checker.model.JavaRule
+import at.fhv.lka2.checker.model.Violation
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
+import java.io.File
+
+data class ClassNameJavaRuleConfig(
+    override val enabled: Boolean = true,
+    val pattern: Regex = "[A-Z][a-zA-Z]*".toRegex()
+) : RuleConfig
+
+class ClassNameJavaRule(config: ClassNameJavaRuleConfig = ClassNameJavaRuleConfig()) :
+    JavaRule<ClassNameJavaRuleConfig>(config) {
+
+    override fun visit(`class`: ClassOrInterfaceDeclaration, arg: MutableList<Violation>) {
+        if (!`class`.nameAsString.matches(config.pattern)) {
+            arg.add(
+                Violation(
+                    Violation.Location(
+                        File(`class`.findCompilationUnit().get().storage.get().fileName),
+                        `class`.begin.get().line
+                    ),
+                    "Class name '${`class`.nameAsString}' does not follow Pattern: ${config.pattern}"
+                )
+            )
+        }
+        super.visit(`class`, arg)
+    }
+}
